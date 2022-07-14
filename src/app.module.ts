36432e03b305d87f990281d5f10544b97d49c6cc
DaisukeMatsuura <mail@brightful.jp>
2022-07-14 14:51:25 +0900

    add: posts module & schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './components/posts/posts.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: path.join(process.cwd(), 'src/generated/graphql/schema.gql'),
+      sortSchema: true,
+      driver: ApolloDriver,
+    }),
+    PostsModule, // これ
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
